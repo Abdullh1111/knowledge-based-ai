04d83d44aa468d635ffd8acc54249f0b678fc438
Abdullh1111 <abdullah4474032@gmail.com>
2026-09-17 11:11:29 +0600

    feat: add vector in pg database

--- agent/src/app.module.ts
+++ agent/src/app.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './libs/prisma/prisma.module';
 import { AiAssistantModule } from './modules/ai-assistant/ai-assistant.module';
 import { FilesModule } from './modules/files/files.module';
+import { PrismaModule } from './libs/prisma/prisma.module';
+import { EmbeddingModule } from './libs/embedding/embedding.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
+    EmbeddingModule,
     AiAssistantModule,
     FilesModule,
-    PrismaModule,
-    PrismaModule,
   ],
   controllers: [],
   providers: [],
--- agent/src/modules/files/files.service.ts
+++ agent/src/modules/files/files.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/libs/prisma/prisma.service';
 
 @Injectable()
-export class FilesService {
-  constructor(private readonly prisma: PrismaService) {}
-}
+export class FilesService {}
